d626ec0992b5c06da4aafe884e7c990966cb8b1a
Glasgalas <alex.starichenko@icloud.com>
2022-06-16 12:04:23 +0300

    add status sort in get request contacts

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -5,17 +5,26 @@ const { Contact } = contactModel;
 
 export const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, status } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner: _id }, "", {
+
+  let resultWithStatus;
+  if (status) {
+    resultWithStatus = await Contact.find({
+      owner: _id,
+      status: status,
+    }).populate("owner", "_id name email");
+  }
+  const resultWithoutStatus = await Contact.find({ owner: _id }, "", {
     skip,
     limit: Number(limit),
   }).populate("owner", "_id name email");
+
   res.json({
     status: "success",
     code: 200,
     data: {
-      result,
+      result: resultWithStatus || resultWithoutStatus,
     },
   });
 };
--- controllers/users/getCurrent.js
+++ controllers/users/getCurrent.js
@@ -1,7 +1,7 @@
-import models from "../../models/index.js";
+// import models from "../../models/index.js";
 
-const { userModel } = models;
-const { User, joiRegisterSchema, joiLoginSchema } = userModel;
+// const { userModel } = models;
+// const { User, joiRegisterSchema, joiLoginSchema } = userModel;
 
 export const getCurrent = async (req, res) => {
   const { name, email } = req.user;
